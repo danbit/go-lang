e2d62228e18796c9008e0ae4d23261eeb9c0fef4
Danilo <daniloesi@gmail.com>
2019-05-20 20:00:26 -0300

    Move snake parts

--- snake2d/main.go
+++ snake2d/main.go
@@ -7,17 +7,32 @@ import (
 )
 
 const (
-	screenWidth  = 640
-	screenHeight = 480
-	fps          = 30
+	ScreenWidth  int32 = 640
+	ScreenHeight int32 = 480
+	CellSize     int32 = 15
+	DELAY              = 300
 )
 
+type Vector2D struct {
+	X, Y int32
+}
+
+type Snake struct {
+	width  int32
+	height int32
+	parts  []Vector2D
+}
+
+// horizontalVelocity, verticalVelocity
+var dX, dY int32
+
 func main() {
-	var winTitle = "Snake 2D"
+	var winTitle = "Go Snake 2D"
 	var window *sdl.Window
 	var context sdl.GLContext
 	var renderer *sdl.Renderer
 	var event sdl.Event
+	var snake Snake
 	var running bool
 	var err error
 
@@ -30,7 +45,7 @@ func main() {
 	window, err = sdl.CreateWindow(
 		winTitle,
 		sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
-		screenWidth, screenHeight,
+		ScreenWidth, ScreenHeight,
 		sdl.WINDOW_OPENGL)
 	if err != nil {
 		fmt.Println("initializing window:", err)
@@ -53,8 +68,19 @@ func main() {
 
 	running = true
 
-	var rectSnake = sdl.Rect{X: screenWidth / 2, Y: screenHeight / 2, W: 15, H: 15}
-	///var rectFood = sdl.Rect{X: 0, Y: 0, W: 5, H: 5}
+	var rectFood = sdl.Rect{X: 0, Y: 0, W: 5, H: 5}
+
+	snake = Snake{
+		width:  15,
+		height: 15,
+		parts: []Vector2D{
+			Vector2D{X: ScreenWidth/2 + CellSize*2 + 1, Y: ScreenHeight / 2},
+			Vector2D{X: ScreenWidth/2 + CellSize + 1, Y: ScreenHeight / 2},
+			Vector2D{X: ScreenWidth / 2, Y: ScreenHeight / 2}},
+	}
+
+	dX = 1
+	dY = 0
 
 	var nextTick uint32
 	for running {
@@ -64,30 +90,30 @@ func main() {
 			case *sdl.QuitEvent:
 				running = false
 			case *sdl.KeyboardEvent:
-				fmt.Println(e)
+				if e.Type == sdl.KEYDOWN {
+					changeDirection(e)
+				}
 			}
 		}
 
 		drawBackground(renderer)
 		renderer.Clear()
 
-		draw(renderer, &rectSnake)
-		move(renderer, &rectSnake)
+		draw(renderer, &rectFood)
+
+		moveSnake(&snake)
+		drawSnake(renderer, &snake)
 
 		renderer.Present()
 
 		//improve preformance
 		ticks := sdl.GetTicks()
-
 		if ticks < nextTick {
 			sdl.Delay(nextTick - ticks)
 		}
-		nextTick = ticks + (1000 / fps)
-
+		nextTick = ticks + (DELAY)
 	}
 }
-
-func update() {}
 
 func drawBackground(renderer *sdl.Renderer) {
 	renderer.SetDrawColor(0, 0, 0, 0)
@@ -98,14 +124,51 @@ func draw(renderer *sdl.Renderer, rect *sdl.Rect) {
 	renderer.FillRect(rect)
 }
 
-func move(renderer *sdl.Renderer, rect *sdl.Rect) {
-	renderer.SetDrawColor(255, 255, 255, 255)
+func drawSnake(renderer *sdl.Renderer, snake *Snake) {
+	for _, element := range snake.parts {
+		draw(renderer, &sdl.Rect{
+			X: element.X, Y: element.Y, W: snake.width, H: snake.height})
+	}
+}
 
-	rect.X++
+func moveSnake(snake *Snake) {
 
-	if rect.X > screenWidth {
-		rect.X = 0
+	//TODO check collision
+
+	for i := 0; i < len(snake.parts)-1; i++ {
+		snake.parts[i] = snake.parts[i+1]
 	}
 
-	renderer.FillRect(rect)
+	tail := snake.parts[len(snake.parts)-1]
+	snake.parts[len(snake.parts)-1].X = tail.X + dX*CellSize
+	snake.parts[len(snake.parts)-1].Y = tail.Y + dY*CellSize
+
+	for i := range snake.parts {
+		if headPos := snake.parts[i]; headPos.X > ScreenWidth {
+			snake.parts[i].X = 0
+		} else if headPos.X <= 0 {
+			snake.parts[i].X = ScreenWidth
+		} else if headPos.Y > ScreenHeight {
+			snake.parts[i].Y = 0
+		} else if headPos.Y <= 0 {
+			snake.parts[i].Y = ScreenHeight
+		}
+	}
+}
+
+func changeDirection(e *sdl.KeyboardEvent) {
+
+	if key := e.Keysym.Sym; key == sdl.K_RIGHT && dX != -1 {
+		dX = 1
+		dY = 0
+	} else if key == sdl.K_LEFT && dX != 1 {
+		dX = -1
+		dY = 0
+	} else if key == sdl.K_UP && dY != 1 {
+		dX = 0
+		dY = -1
+	} else if key == sdl.K_DOWN && dY != -1 {
+		dX = 0
+		dY = 1
+	}
 }
