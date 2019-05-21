3d4ef60a28e03941d0c864519f04fdbcc2949474
Danilo <daniloesi@gmail.com>
2019-05-21 19:24:53 -0300

    Draw random food

--- snake2d/main.go
+++ snake2d/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"math/rand"
+	"time"
 
 	"github.com/veandco/go-sdl2/sdl"
 )
@@ -10,17 +13,18 @@ const (
 	ScreenWidth  int32 = 640
 	ScreenHeight int32 = 480
 	CellSize     int32 = 15
-	DELAY              = 300
 )
 
-type Vector2D struct {
-	X, Y int32
+type Snake struct {
+	width     int32
+	height    int32
+	positions []sdl.Point
 }
 
-type Snake struct {
-	width  int32
-	height int32
-	parts  []Vector2D
+type Food struct {
+	width    int32
+	height   int32
+	position sdl.Point
 }
 
 // horizontalVelocity, verticalVelocity
@@ -33,6 +37,7 @@ func main() {
 	var renderer *sdl.Renderer
 	var event sdl.Event
 	var snake Snake
+	var food Food
 	var running bool
 	var err error
 
@@ -68,21 +73,30 @@ func main() {
 
 	running = true
 
-	var rectFood = sdl.Rect{X: 0, Y: 0, W: 5, H: 5}
-
 	snake = Snake{
-		width:  15,
-		height: 15,
-		parts: []Vector2D{
-			Vector2D{X: ScreenWidth/2 + CellSize*2 + 1, Y: ScreenHeight / 2},
-			Vector2D{X: ScreenWidth/2 + CellSize + 1, Y: ScreenHeight / 2},
-			Vector2D{X: ScreenWidth / 2, Y: ScreenHeight / 2}},
+		width:  CellSize,
+		height: CellSize,
+		positions: []sdl.Point{
+			sdl.Point{X: ScreenWidth/2 + CellSize*4 + 1, Y: ScreenHeight / 2},
+			sdl.Point{X: ScreenWidth/2 + CellSize*3 + 1, Y: ScreenHeight / 2},
+			sdl.Point{X: ScreenWidth/2 + CellSize*2 + 1, Y: ScreenHeight / 2},
+			sdl.Point{X: ScreenWidth/2 + CellSize + 1, Y: ScreenHeight / 2},
+			sdl.Point{X: ScreenWidth / 2, Y: ScreenHeight / 2}},
 	}
+
+	food = Food{
+		width:    CellSize,
+		height:   CellSize,
+		position: randomPosition(),
+	}
+
+	fmt.Printf("food.position= %d, %d\n", food.position.X, food.position.Y)
 
 	dX = 1
 	dY = 0
 
-	var nextTick uint32
+	ticker := time.NewTicker(time.Second / 20)
+
 	for running {
 
 		for event = sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
@@ -99,19 +113,14 @@ func main() {
 		drawBackground(renderer)
 		renderer.Clear()
 
-		draw(renderer, &rectFood)
-
 		moveSnake(&snake)
+
+		drawFood(renderer, &food)
 		drawSnake(renderer, &snake)
 
 		renderer.Present()
 
-		//improve preformance
-		ticks := sdl.GetTicks()
-		if ticks < nextTick {
-			sdl.Delay(nextTick - ticks)
-		}
-		nextTick = ticks + (DELAY)
+		<-ticker.C
 	}
 }
 
@@ -125,33 +134,53 @@ func draw(renderer *sdl.Renderer, rect *sdl.Rect) {
 }
 
 func drawSnake(renderer *sdl.Renderer, snake *Snake) {
-	for _, element := range snake.parts {
+	for _, element := range snake.positions {
 		draw(renderer, &sdl.Rect{
 			X: element.X, Y: element.Y, W: snake.width, H: snake.height})
 	}
+}
+
+func drawFood(renderer *sdl.Renderer, food *Food) {
+	draw(renderer, &sdl.Rect{
+		X: food.position.X, Y: food.position.Y, W: food.width, H: food.height})
+}
+
+func randomPosition() sdl.Point {
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+
+	newX := math.Floor(r1.Float64()*(float64(ScreenWidth/CellSize))) * float64(CellSize)
+	newY := math.Floor(r1.Float64()*(float64(ScreenWidth/CellSize))) * float64(CellSize)
+
+	newPosition := sdl.Point{
+		X: int32(newX),
+		Y: int32(newY),
+	}
+
+	return newPosition
 }
 
 func moveSnake(snake *Snake) {
 
 	//TODO check collision
 
-	for i := 0; i < len(snake.parts)-1; i++ {
-		snake.parts[i] = snake.parts[i+1]
+	for i := 0; i < len(snake.positions)-1; i++ {
+		snake.positions[i] = snake.positions[i+1]
 	}
 
-	tail := snake.parts[len(snake.parts)-1]
-	snake.parts[len(snake.parts)-1].X = tail.X + dX*CellSize
-	snake.parts[len(snake.parts)-1].Y = tail.Y + dY*CellSize
+	head := snake.positions[len(snake.positions)-1]
+	snake.positions[len(snake.positions)-1].X = head.X + dX*CellSize
+	snake.positions[len(snake.positions)-1].Y = head.Y + dY*CellSize
 
-	for i := range snake.parts {
-		if headPos := snake.parts[i]; headPos.X > ScreenWidth {
-			snake.parts[i].X = 0
-		} else if headPos.X <= 0 {
-			snake.parts[i].X = ScreenWidth
+	for i := range snake.positions {
+		if headPos := snake.positions[i]; headPos.X > ScreenWidth {
+			snake.positions[i].X = 0
+		} else if headPos.X < 0 {
+			snake.positions[i].X = ScreenWidth
 		} else if headPos.Y > ScreenHeight {
-			snake.parts[i].Y = 0
-		} else if headPos.Y <= 0 {
-			snake.parts[i].Y = ScreenHeight
+			snake.positions[i].Y = 0
+		} else if headPos.Y < 0 {
+			snake.positions[i].Y = ScreenHeight
 		}
 	}
 }
